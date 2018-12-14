de3b27b9546a42205d0651612c124136fa5b6b72
Pavillin <dylansprague11@hotmail.com>
2018-12-14 13:39:50 -0500

    CarLot almost done

---
+++ src/CarLot.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class CarLot {
+    private TreeMap<String, TreeSet<Car>> menu = CarLoader.loadCars();
+
+    public CarLot(TreeMap<String, TreeSet<Car>> menu) {
+        this.menu = menu;
+    }
+
+    public ArrayList getBrands(){
+        ArrayList<String> brands = new ArrayList<>();
+        String all = "All Brands";
+
+        brands.add(all);
+        brands.addAll(menu.keySet());
+        return brands;
+    }
+
+    public TreeSet<Car> getCars(String brand){
+        TreeSet<Car> cars = new TreeSet<>();
+        if (menu.containsKey(brand) && !brand.equals("All Brands")){
+            cars.addAll(menu.get(brand));
+            return cars;
+        }else{
+            cars.addAll(menu.get("All Brands"));
+            return cars;
+        }
+    }
+
+    public void sellCar(Car car){
+        if (menu.containsKey(car.getBrand())){
+            menu.remove(car);
+        }
+    }
+
+    /*public double getInventoryValue(String brand){
+        Double val;
+        if (brand.equals("All Brands")){
+            val = menu.values().stream()
+                    .mapToDouble(c -> c.getPrice())
+                    .sum();
+        }else {
+            val = menu.get(brand).stream()
+                    .mapToDouble(c -> c.getPrice())
+                .sum();
+        }
+        return val;
+    }*/
+}
